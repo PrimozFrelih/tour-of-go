6ec34a3480e3b2eb43f17056d8b47448b331c7da
matijavizintin <matija.vizintin@gmail.com>
2016-02-13 18:25:24 +0100

    playground

--- main/playground.go
+++ main/playground.go
@@ -1,36 +1,6 @@
 package main
 
-
 func main() {
-	ch := make(chan int)
-	batteryConsumer()
-	<-ch
-}
-
-const THREADS = 10
-
-func batteryConsumer() {
-	for i := 0; i < THREADS; i++ {
-		go func() {
-			counter := 0
-			lowerLimit := 0
-			upperLimit := 0
-
-			var increment bool
-			for {
-				if counter <= lowerLimit {
-					increment = true
-				} else if counter >= upperLimit {
-					increment = false
-				}
-
-				if increment {
-					counter++
-				} else {
-					counter--
-				}
-			}
-
-		}()
-	}
+	// this function can be executed as a standalone app using 'go run playground.go'
+	// you can put here any code and execute it directly
 }
